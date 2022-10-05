b031737fa88c37b5d0fd93dbe0003ab31d084ca8
Cong <conbo1102a@gmail.com>
2022-10-05 09:28:41 +0700

    update project

--- app/src/main/java/com/android/baitap/MainActivity.java
+++ app/src/main/java/com/android/baitap/MainActivity.java
@@ -12,7 +12,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText userName, passWord;
-    TextView signInBtn;
+    TextView loginBtn;
     CheckBox rememberCb;
 
     SharedPreferences sharedPreferences;
@@ -22,22 +22,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        mapping();
         sharedPreferences = getSharedPreferences("dataLogin", MODE_PRIVATE);
 
         //  Lấy giá trị
-        userName.setText(sharedPreferences.getString("taiKhoan",""));
+        userName.setText(sharedPreferences.getString("taikhoan", ""));
+        passWord.setText(sharedPreferences.getString("matkhau", ""));
+        rememberCb.setChecked(sharedPreferences.getBoolean("checked", false));
 
-        mapping();
-        signInBtn.setOnClickListener(new View.OnClickListener() {
+        loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String username = userName.getText().toString().trim();
                 String password = passWord.getText().toString().trim();
 
                 if(username.equals("admin1") && password.equals("admin")) {
-                    Toast.makeText(MainActivity.this, "Đăng nhập thành công", Toast.LENGTH_SHORT);
+                    Toast.makeText(MainActivity.this, "Đăng nhập thành công", Toast.LENGTH_SHORT).show();
 
-                    // If check ?
+//                     If check ?
                     if(rememberCb.isChecked()) {
                         SharedPreferences.Editor editor = sharedPreferences.edit();
                         editor.putString("taikhoan", username);
@@ -46,14 +49,14 @@ public class MainActivity extends AppCompatActivity {
                         editor.commit();
                     }
                 } else {
-                    Toast.makeText(MainActivity.this, "Lỗi đăng nhậ", Toast.LENGTH_SHORT);
+                    Toast.makeText(MainActivity.this, "Lỗi đăng nhập", Toast.LENGTH_SHORT).show();
                 }
             }
         });
     }
 
     private void mapping() {
-        signInBtn = (TextView) findViewById(R.id.signInBtn);
+        loginBtn = (TextView) findViewById(R.id.signInBtn) ;
         userName = (EditText) findViewById(R.id.userName);
         passWord = (EditText) findViewById(R.id.password);
         rememberCb = (CheckBox) findViewById(R.id.rememberCheckbox);
